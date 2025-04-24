65c7904de0f467bc38c24072ba3f125d0d294cb4
Léonard <Leeooonaard@gmail.com>
2025-04-24 21:20:09 +0800

    CMake: use CMake itself instead of Python for platform-specific details in config script.
    Leveraging CMake only makes sense as it was literally designed for this.
    It was also possible to provide a bunch of variables to the Python script instead (CMAKE_IMPORT_LIBRARY_SUFFIX and a bunch more) but then CMake itself uses them in its own commands.

--- cmake/python_cmake_config.py
+++ cmake/python_cmake_config.py
@@ -4,7 +4,6 @@
 import argparse
 import sys
 import os
-import glob
 import re
 import tomllib
 
@@ -39,63 +38,6 @@ def find_module(module):
     print(f'Unable to find module {module}.')
     return None
 
-def shiboken_include():
-    module = find_module('shiboken6_generator')
-    if module is None:
-        return None
-
-    result = os.path.join(module, 'include')
-    if not os.path.exists(result):
-        print(f'Unable to find include directory for module {module}.')
-        return None
-
-    return result
-
-def shiboken_library():
-    module = find_module('shiboken6')
-    if module is None:
-        return None
-
-    suffix = ''
-    if sys.platform == 'win32':
-        suffix = 'lib'
-    elif sys.platform == 'darwin':
-        suffix = 'dylib'
-    # Linux
-    else:
-        suffix = 'so.*'
-    
-    pattern = '*.' + suffix
-    if sys.platform != 'win32':
-        pattern = 'lib' + pattern
-
-    result = glob.glob(os.path.join(module, pattern))
-    def predicate(lib):
-        basename = os.path.basename(lib)
-        if 'shiboken' in basename or 'pyside6' in basename:
-            return True
-        return False
-    result = [lib for lib in result if predicate(lib)]
-
-    if sys.platform == 'win32':
-        result = [os.path.realpath(lib) for lib in result]
-
-    return ';'.join(result)
-
-def shiboken_generator():
-    module = find_module('shiboken6_generator')
-    if module is None:
-        return None
-
-    result = os.path.join(module, 'shiboken6')
-    if sys.platform == 'win32':
-        result += '.exe'
-
-    if not os.path.exists(result):
-        print('Unable to locate Shiboken generator')
-        return None
-    return result
-
 
 def kwpackval(key, *keys): return {k: key for k in keys}
 subparser_props = { 'title': 'queries',
@@ -119,45 +61,32 @@ subparser.add_parser('deps',
                      **kwpackval("query the project's direct dependencies",
                                  'help',
                                  'description')
-                     ).set_defaults(func=python_project_dependencies,
+                     ).set_defaults(func=lambda _: python_project_dependencies(),
                                     err=error_pyproject)
 subparser.add_parser('devdeps',
                      **kwpackval("query the project's development dependencies",
                                  'help',
                                  'description')
-                     ).set_defaults(func=python_project_dev_dependencies,
+                     ).set_defaults(func=lambda _: python_project_dev_dependencies(),
                                     err=error_pyproject)
 
 error_module = ('Did you forget to activate your virtualenv? Or perhaps'
                 ' you forgot to build / install PySide6 into your currently active Python'
                 ' environment?')
-subparser = queries.add_parser('shiboken',
-                               **kwpackval("queries information about Shiboken",
+subparser = queries.add_parser('module',
+                               **kwpackval("queries the path for a given Python module",
                                            'help',
-                                           'description')
-                               ).add_subparsers(**subparser_props)
-subparser.add_parser('generator',
-                     **kwpackval("query the Shiboken generator path",
-                                 'help',
-                                 'description')
-                     ).set_defaults(func=shiboken_generator,
-                                    err=error_module)
-subparser.add_parser('include',
-                     **kwpackval("query Shiboken's include directories needed for its generated files",
-                                 'help',
-                                 'description')
-                     ).set_defaults(func=shiboken_include,
-                                    err=error_module)
-subparser.add_parser('libraries',
-                     **kwpackval("query Shiboken's libraries needed for linking its generated files",
-                                 'help',
-                                 'description')
-                     ).set_defaults(func=shiboken_library,
-                                    err=error_module)
+                                           'description'))
+subparser.add_argument('which',
+                       help='which module to query for',
+                       choices=['shiboken6',
+                                'shiboken6_generator'])
+subparser.set_defaults(func=lambda args: find_module(args.which),
+                       err=error_module)
 
 
 args = parser.parse_args()
-res = args.func()
+res = args.func(args)
 if res is None:
     sys.exit(args.err)
 print(res)
